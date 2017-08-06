febac97ffa50c648ce731a09902f576b6e630a43
pavanpeketi <30745337+pavanpeketi@users.noreply.github.com>
2017-08-06 13:02:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/articleone', function (req, res) {
-  res.send("Article one requested and will be served here.");
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/articletwo', function (req, res) {
